bd8afb47f23225a5ef248bf1de363aa3bdd4c56e
Igor <igordevmail@gmail.com>
2016-03-20 16:36:19 +0200

    clear code

--- main.c
+++ main.c
@@ -480,19 +480,5 @@ int main()
 
     }
 
-//    pthread_t threads[NUM_THREADS];
-//      int rc;
-//      long t;
-//      for(t=0; t<NUM_THREADS; t++){
-//         printf("In main: creating thread %ld\n", t);
-//         rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-//         if (rc){
-//            printf("ERROR; return code from pthread_create() is %d\n", rc);
-//            exit(-1);
-//         }
-//      }
-
-//      /* Last thing that main() should do */
-    pthread_exit(NULL);
     return 0;
 }
